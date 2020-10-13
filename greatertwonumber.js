40e4f2884bd5cdbf84db4066220b15d7c30b87e0
Md Nawazish Alam <58005159+nawazish0230@users.noreply.github.com>
2020-10-13 23:05:44 +0530

    Create greatertwonumber.js

---
+++ greatertwonumber.js
@@ -0,0 +1,14 @@
+function greater(x, y){	
+
+if(x === y){
+return "Numbers are the same";
+}else if (x > y){
+return x;
+}else{
+return y;
+}
+}else{
+return "Numbers don't fit in range";
+}
+}
+console.log(greater(45, 60));
